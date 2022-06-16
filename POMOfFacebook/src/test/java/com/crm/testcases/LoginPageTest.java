34922825b93f40afa19b09819ffaeaf0e03bf882
shubhangi1208 <96242483+shubhangi1208@users.noreply.github.com>
2022-06-16 08:19:36 +0530

    ADDED:POM for Facebook

--- POMOfFacebook/src/test/java/com/crm/testcases/LoginPageTest.java
+++ POMOfFacebook/src/test/java/com/crm/testcases/LoginPageTest.java
@@ -3,10 +3,12 @@ package com.crm.testcases;
 import com.crm.base.TestBase;
 import com.crm.pages.HomePage;
 import com.crm.pages.LoginPage;
+import com.crm.util.TestUtil;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.logging.Logger;
@@ -14,10 +16,13 @@ import java.util.logging.Logger;
 public class LoginPageTest extends TestBase {
     LoginPage loginPage;
     HomePage homePage;
+    String sheetName = "contacts";
+    Logger log = Logger.getLogger("LoginPageTest.class");
 
     public LoginPageTest() {
         super();// super keyword calls
     }
+    WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
@@ -27,6 +32,7 @@ public class LoginPageTest extends TestBase {
 
     @Test(priority = 1)
     public void loginPageTitleTest() {
+        log.info("****************************** Facebook Title *****************************************");
         String title = loginPage.validateLoginPageTitle();
         Assert.assertEquals(title, "Log in to Facebook");
     }
@@ -39,7 +45,18 @@ public class LoginPageTest extends TestBase {
 
     @Test(priority = 3)
     public void loginTest() {
+
         homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    }
+
+    @DataProvider
+    public Object[][] getCRMTestData(){
+        Object data[][] = TestUtil.getTestData(sheetName);
+        return data;
+    }
+    @Test(priority=4,dataProvider="getCRMTestData")
+    public void logintoFacebook(String username, String password){
+       loginPage.login(username, password);
     }
 
     @AfterMethod
